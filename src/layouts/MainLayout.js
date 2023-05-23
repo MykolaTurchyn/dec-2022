3875b0bfc9df85402f54593f8799048d268355fd
MykolaTurchyn <tyrchnmikigt@gmail.com>
2023-05-23 21:13:28 +0300

    test

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from "../components/header/Header";
-import {Navigate} from "react-router-dom";
 
 const MainLayout = () => {
     return (
         <div>
             <Header/>
-            <button><Navigate to={'users'}/> users</button>
         </div>
     );
 };
